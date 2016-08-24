d1fbafe046a601c6b8f4de40ba77384ca775a4dc
Jackson Meyer and Kiet Duong <jackson@>
2016-08-24 16:29:01 -0700

    added more js function and completed player 1 buttons and stuff

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,8 @@ var roll = function () {
 
 }
 
-overalltotal1 = [];
+overalltotal = [];
 player1TurnRolls = [];
-
 
 var turns = function() {
   var total = 0;
@@ -18,60 +17,42 @@ var turns = function() {
 return total;
 }
 
+var totaltotal = function() {
+  var total2 = 0;
+  overalltotal.forEach(function(data) {
+  total2 += data;
+});
+
+return total2;
+}
+
 
 
 //front end
 $(document).ready(function() {
   $("#roll1").click(function(event) {
+    event.preventDefault();
     player1TurnRolls.push(roll());
     p1reversed = player1TurnRolls.reverse();
     if (p1reversed[0] === 1) {
-      alert("your turn over!")
+      alert("you rolled a 1 sucker, your turn over!")
       player1TurnRolls = [];
-
 }
-    console.log(player1TurnRolls)
 
 
     $(".currentRoll1").text("Rolls: " + player1TurnRolls + ",");
-
     $(".turnTotal1").text("turn total: " + turns())
 
-    event.preventDefault();
+
 
   })
     $("#hold1").click(function(event) {
-      overalltotal1.push(turns());
-    $(".overalltotal1").text("Overall total: " + overalltotal1);
+      overalltotal.push(turns());
+    $(".overalltotal1").text("Overall total: " + totaltotal());
+    alert("you held, your turn is now over!")
     player1TurnRolls = [];
+
+
 })
 
 });
-
-// turnTotal = roll1 + roll2 + roll3 + roll4...ect
-// Overall Total = turnTotal1 + turnTotal2 + turnTotal3....ect
-
-// if (turn total >= "100") {
-//   alert("You won!");
-// }
-// if(itemInput.length === 0){
-//   alert("Please enter something");
-//   return;
-// roll();
-// 6
-// player1TurnRolls = [];
-// []
-// player1TurnRolls.push(roll());
-// 1
-// player1TurnRolls.push(roll());
-// 2
-// player1TurnRolls.push(roll());
-// 3
-// player1TurnRolls.push(roll());
-// 4
-// player1TurnRolls.push(roll());
-// 5
-// player1TurnRolls;
-// [1, 4, 4, 2, 6]
-// player1TurnRolls.join();
-// "1,4,4,2,6"
