6d1d479e11666367730c4cf1735a7a90f2b1a730
Jackson Meyer and Kiet Duong <jackson@>
2016-08-24 15:11:37 -0700

    add some javascript, turn total and roll total

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,19 @@
 //back end
-turnTotalArray = [];
-overallTotalArray = [];
-
-
-
-var rando = function () {
+var roll = function () {
   return Math.floor(Math.random() * 6)
   + 1;
+
+}
+
+player1TurnRolls = [];
+
+var turns = function() {
+  var total = 0;
+  player1TurnRolls.forEach(function(data) {
+  total += data;
+});
+
+return total;
 }
 
 
@@ -14,12 +21,51 @@ var rando = function () {
 //front end
 $(document).ready(function() {
   $("#roll1").click(function(event) {
-    $(".currentRoll1").append(" " + rando() );
-    turnTotalArray.push();
+    player1TurnRolls.push(roll());
+    if (player1TurnRolls[-1] === "1") {
+      alert("turn over!")
+    }
+
+
+
+    $(".currentRoll1").text("Rolls: " + player1TurnRolls + ",");
+
+
+    $(".turnTotal1").text("turn total: " + turns())
+
+
 
 
 
     event.preventDefault();
-  })
 
+  })
 });
+
+// turnTotal = roll1 + roll2 + roll3 + roll4...ect
+// Overall Total = turnTotal1 + turnTotal2 + turnTotal3....ect
+
+// if (turn total >= "100") {
+//   alert("You won!");
+// }
+// if(itemInput.length === 0){
+//   alert("Please enter something");
+//   return;
+// roll();
+// 6
+// player1TurnRolls = [];
+// []
+// player1TurnRolls.push(roll());
+// 1
+// player1TurnRolls.push(roll());
+// 2
+// player1TurnRolls.push(roll());
+// 3
+// player1TurnRolls.push(roll());
+// 4
+// player1TurnRolls.push(roll());
+// 5
+// player1TurnRolls;
+// [1, 4, 4, 2, 6]
+// player1TurnRolls.join();
+// "1,4,4,2,6"
